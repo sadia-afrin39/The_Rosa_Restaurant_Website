4a75e34ed3e86b855aa449d96106fcbd21064d12
sadiaTarin <tarin.cse5.bu@gmail.com>
2022-10-02 21:37:19 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+/*select elements*/
+const menuToggler = document.querySelector('.menu-toggle');
+const body = document.querySelector('body');
+
+menuToggler.addEventListener('click',()=>{
+	body.classList.toggle('open');
+});
+
+
+/*scroll reveal */
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left',{
+	origin:'left',
+	duration:1000,
+	distance:'25rem',
+	delay:300
+});
+
+sr.reveal('.animate-right',{
+	origin:'right',
+	duration:1000,
+	distance:'25rem',
+	delay:600
+});
+
+sr.reveal('.animate-top',{
+	origin:'top',
+	duration:1000,
+	distance:'25rem',
+	delay:600
+});
+
+sr.reveal('.animate-bottom',{
+	origin:'bottom',
+	duration:1000,
+	distance:'25rem',
+	delay:600
+});
